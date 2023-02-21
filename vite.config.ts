8314999f7ea2a8718b07e6d3538d61dbd6dd671d
BDK27 <briansam195@gmail.com>
2023-02-21 14:43:01 +0800

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'vite_education',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
